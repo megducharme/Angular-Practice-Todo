7686e9a8fb6a12760748eaedcf4a27edfffcc129
Meg Ducharme <ducharme.meg@gmail.com>
2016-07-14 11:11:36 -0500

    Added JS

--- script.js
+++ script.js
@@ -1,1 +1,91 @@
-console.log("Meg")
+// // console.log("Meg")
+
+// var secondsMinute = 60;
+// var minutesHour = 60;
+// var hoursDay = 24;
+// var daysYear = 365;
+// var megAge = 28;
+// var megAgeMinutes = (megAge*daysYear*hoursDay*minutesHour*secondsMinute)
+
+// console.log(megAgeMinutes)
+
+// if (megAgeMinutes >= 138267) {
+//   alert("I'm old!");
+// } else if (megAgeMinutes <= 147283) {
+//   alert("I am young!")
+// }
+
+
+// for (var i = 0; i < 100; i += 10) {
+//   console.log(i);
+// }
+
+// var newArray = []
+// for (var i = 14682; i >= 345; i /= 2) {
+//   console.log(i);
+//   newArray.push(i);
+//   console.log(newArray);
+// }
+
+
+// for (var newArray = [], i = 100; i >= 0; i -= 1) {
+//   if (i%2 === 0) {
+//     newArray.unshift(i);
+//   } else {
+//     newArray.push(i);
+//   }
+//   console.log(newArray)
+// }
+
+
+// var phrase = "The lazy dog";
+// var newPhrase = phrase.replace("lazy", "bounding");
+// alert(newPhrase);
+
+// var phrase = "I love JavaScript!";
+// var newPhrase = phrase.replace("I love JavaScript", "This is cool!");
+// alert(newPhrase);
+
+
+
+var phrase = "Hey, look at me!";
+var element = document.getElementById("container");
+element.innerHTML = phrase;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
