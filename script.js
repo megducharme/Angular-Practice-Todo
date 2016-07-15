dac4a3778d4b1a9cff93ae067fa8b397293a604f
Meg Ducharme <ducharme.meg@gmail.com>
2016-07-15 14:09:06 -0500

    Added more exercises

--- script.js
+++ script.js
@@ -46,11 +46,68 @@
 // var newPhrase = phrase.replace("I love JavaScript", "This is cool!");
 // alert(newPhrase);
 
+// var phrase = "Hey, look at me!";
+// var element = document.getElementById("container");
+// element.innerHTML = phrase;
 
 
-var phrase = "Hey, look at me!";
-var element = document.getElementById("container");
-element.innerHTML = phrase;
+
+// // function range (num) {
+// //   var array = []
+// //   for (var i = 0; i < num; i++) {
+// //     array.push(i)
+// //   }
+// //    return array
+// // }
+
+// console.log("#1:", range(5))
+
+
+// function rangeObject (num, value) {
+//   var obj = {}
+//   for (var i = 0; i < num; i++)
+//     obj[i] = value;
+//     return obj
+// }
+
+// console.log( "#2", rangeObject(9, 6))
+
+// Alpha stacker
+
+// var letters = ['a','b','c','d','e','f','g','h','i','j','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+// var newString = "";
+
+// function stackLetters () {
+//   for (var i = 0; i < letters.length; i++) {
+//     if (i % 5 === 0 && i != 0) {
+//       newString += " ";
+//     }
+//       newString += letters[i];
+//       console.log(newString);
+//     }
+//   }
+
+// stackLetters ();
+
+
+// function letterStacker
+
+
+//Colored Reindeer Exercise
+
+
+
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+var hohohoElement = document.getElementById("reindeer");
+
+for (var i = 0 ; i < reindeer.length; i++) {
+  // console.log(colors[i] + " " + reindeer [i]);
+  // console.log(hohohoElement.innerHTML);
+  hohohoElement.innerHTML += "<div>" colors[i] + " " + reindeer[i] + "</div>";
+}
 
 
 
